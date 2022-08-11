f247b8473ab803a9f9416c6ebf5c4e0fafe074d5
Rafael <rafaelvrcr@gmail.com>
2022-08-10 22:52:44 -0300

    [Feat] Page Quem somos

---
+++ page-quem-somos.php
@@ -0,0 +1,79 @@
+<?php // Template Name: Quem somos ?>
+
+<?php get_header(); ?>
+  <section class="container-banner-quem-somos">
+    <div class="banner-title">
+      <h1>Quem somos</h1>
+    </div>
+    <div class="banner-quem-somos">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/quem-somos.jpg" alt="">
+      <h1>Telerradiologia com alto padrão de qualidade</h1>
+    </div>
+  </section>
+
+  <section class="quem-somos-content">
+    <div class="container-1">
+      <p>
+        A Weblaudo surge da união de médicos experientes e renomados na área da radiologia 
+        para aperfeiçoar o atendimento no cenário da telerradiologia nacional. Com um 
+        tratamento humanizado, vamos além da emissão de laudos com responsabilidade, prestando 
+        consultoria e priorizando o suporte aos médicos. Trabalhamos com o melhor aparato 
+        tecnológico em transmissões de imagens, atendendo desdes hospitais até clínicas de 
+        imagem, particulares ou públicas. Essas características são essenciais na hora de apresentar 
+        e executar soluções para os nossos clientes, permitindo agir com rapidez e excelência ao 
+        ampliar e desafogar a avaliação de relatórios radiológicos do serviço.
+      </p>
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/quem-somos-1.jpg" alt="">
+    </div>
+    <div class="container-2">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/quem-somos-2.jpg" alt="">
+      <p> 
+        Nossa equipe de radiologistas possui vasta experiência profissional nas diferentes 
+        subespecialidades da radiologia, tais como radiologia de cabeça e pescoço; neurorradiologia 
+        musculoesquelética, pediátrica, tóraco-abdominal e cardiovascular. Todos os radiologistas 
+        da Weblaudo possuem títulos de especialistas concedidos Colégio Brasileiro de Radiologia 
+        (CBR), cumprindo rigorosamente os padrões nacionais de qualidade para trabalhar em parceria 
+        com os serviços de imagens clínicas e hospitais de qualquer lugar no Brasil.
+      </p>
+      
+    </div>
+    <div class="card-conteiner-quem-somos">
+      <div class="card-quem-somos-1">
+        <div class="text-container-somos">
+          <h2 class="como-h2">Como</h2>
+          <h2 class="strong-h2">funciona</h2>
+          <div class="como-funciona-line"></div>
+        </div>
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/quem-somos-3.jpg" alt="">
+        <p>
+          Além de oferecer soluções em serviços de telerradiologia, nós treinamos sua equipe para 
+          o manuseio dos programas e nos predispomos a ajudar no que for preciso para garantir total 
+          confiabilidade no trabalho proposto. Vamos trabalhar para manter o alto nível de qualidade
+          dos seus exames, garantindo a rapidez da entrega. Nós temos uma equipe preparada sempre 
+          pronta para atender você do primeiro ao último dia do ano. Atuando sempre com rigor e 
+          disciplina na avaliação dos laudos médicos.
+        </p>
+      </div>
+      <div class="card-quem-somos-2">
+        <div class="text-container-somos">
+          <h2 class="como-h2">Nossa</h2>
+          <h2 class="strong-h2">tecnologia</h2>
+          <div class="como-funciona-line"></div>
+        </div>
+        <p>
+          Temos a mais avançada tecnologia e contamos com um sistema próprio para garantir segurança, 
+          qualidade, rapidez e eficiência na telerradiologia. Nossos servidores são capacitados, garantindo 
+          a segurança dos dados de forma criptografada. O nosso sistema é de fácil aprendizado e altamente 
+          intuitivo, agilizando o fluxo de registro; conversão e envio de imagens; recuperação, análise e emissão 
+          de laudos. Atuando nesse sistema, que é utilizado por gestores, médicos e radiologistas, fazemos a 
+          conexão entre os radiologistas e os profissionais do seu corpo clínico: monitorando o tráfego de 
+          informações e catalogando todos os exames de forma lógica e organizada. A Weblaudo conta com um canal 
+          direto para que os nossos radiologistas possam trocar experiências e fomentar o estudo em torno de casos 
+          raros e específicos. Isto enriquece ainda mais o nosso trabalho em equipe.
+        </p>
+      </div>
+    </div>
+  </section>
+
+
+<?php get_footer(); ?>
