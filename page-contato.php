c6ca5f45bf3f85ac1ba0a6af6758e6957f4f8d01
Rafael <rafaelvrcr@gmail.com>
2022-08-16 13:50:38 -0300

    [Fix] Dropdown bar logics // Still need css

--- page-contato.php
+++ page-contato.php
@@ -2,50 +2,51 @@
 ?>
 
 <?php get_header(); ?>
-<section class="container-banner-contato">
-  <div class="banner-title">
-    <h1>Fale conosco</h1>
-  </div>
-</section>
-<section class="container-contato">
-  <div class="adress-container">
-    <h3>Weblaudo</h3>
-    <div class="adress-content">
-      <h4>Endereço</h4>
-      <p>Rua Dr. Francisco Matos, 222</p>
-      <p>Boa Viagem, Recife/PE, Brasil</p>
-      <p>CEP 52010-000</p>
+<main class="contato">
+  <section class="container-banner-contato">
+    <div class="banner-title">
+      <h1>Fale conosco</h1>
     </div>
-    <div class="funcionamento-container">
-      <h4>Horário de Funcionamento:</h4>
-      <p>Segunda a sexta das 8h às 18:00</p>
-      <p>Sábado das 8h às 12:00</p>
+  </section>
+  <section class="container-contato">
+    <div class="adress-container">
+      <h3>Weblaudo</h3>
+      <div class="adress-content">
+        <h4>Endereço</h4>
+        <p>Rua Dr. Francisco Matos, 222</p>
+        <p>Boa Viagem, Recife/PE, Brasil</p>
+        <p>CEP 52010-000</p>
+      </div>
+      <div class="funcionamento-container">
+        <h4>Horário de Funcionamento:</h4>
+        <p>Segunda a sexta das 8h às 18:00</p>
+        <p>Sábado das 8h às 12:00</p>
+      </div>
+      <div class="fone-container">
+        <h4>Telefones:</h4>
+        <h5>(81) 3679-5772</h5>
+        <h5>(81)9 9879-5772</h5>
+      </div>
+      <div class="comercial-container">
+        <h4>Comercial:</h4>
+        <h5>(81) 3879-5772</h5>
+        <a href="mailto:webmaster@example.com">contato@weblaudo.com.br</a>
+      </div>
     </div>
-    <div class="fone-container">
-      <h4>Telefones:</h4>
-      <h5>(81) 3679-5772</h5>
-      <h5>(81)9 9879-5772</h5>
+    <div class="map-container">
+      <iframe class="map-medium" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d783.8079148567223!2d-34.89237418717859!3d-8.08676196716734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab1f6aa3f99e1f%3A0x129972e2cea3bdbd!2sWeblaudo%20Teleradiologia!5e0!3m2!1spt-BR!2sbr!4v1660239418827!5m2!1spt-BR!2sbr" width="300" height="550" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <iframe class="map-big" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d783.8079148567223!2d-34.89237418717859!3d-8.08676196716734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab1f6aa3f99e1f%3A0x129972e2cea3bdbd!2sWeblaudo%20Teleradiologia!5e0!3m2!1spt-BR!2sbr!4v1660239418827!5m2!1spt-BR!2sbr" width="450" height="550" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
-    <div class="comercial-container">
-      <h4>Comercial:</h4>
-      <h5>(81) 3879-5772</h5>
-      <a href="mailto:webmaster@example.com">contato@weblaudo.com.br</a>
-    </div>
-  </div>
-  <div class="map-container">
-    <iframe class="map-medium" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d783.8079148567223!2d-34.89237418717859!3d-8.08676196716734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab1f6aa3f99e1f%3A0x129972e2cea3bdbd!2sWeblaudo%20Teleradiologia!5e0!3m2!1spt-BR!2sbr!4v1660239418827!5m2!1spt-BR!2sbr" width="300" height="550" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    <iframe class="map-big" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d783.8079148567223!2d-34.89237418717859!3d-8.08676196716734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab1f6aa3f99e1f%3A0x129972e2cea3bdbd!2sWeblaudo%20Teleradiologia!5e0!3m2!1spt-BR!2sbr!4v1660239418827!5m2!1spt-BR!2sbr" width="450" height="550" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-  </div>
-  <div class="contato-form-container">
-    <h4>
-      Contatos, dúvidas e sugestões, fale com </br>
-      nossa equipe e em breve retornaremos.
-    </h4>
-    <form method="post" action="<?php echo get_template_directory_uri(); ?>/enviar.php">
-      
+    <div class="contato-form-container">
+      <h4>
+        Contatos, dúvidas e sugestões, fale com </br>
+        nossa equipe e em breve retornaremos.
+      </h4>
+      <form method="post" action="<?php echo get_template_directory_uri(); ?>/enviar.php">
+
         <input type="text" name="nome" id="nome-ct" placeholder="Nome">
         <input type="email" name="email" id="email-ct" placeholder="E-mail">
-        <input type="tel" name="tel" id="tel-ct"  placeholder="Fone / Celular">
+        <input type="tel" name="tel" id="tel-ct" placeholder="Fone / Celular">
         <input type="text" name="endereco" id="endereco-ct" placeholder="Endereço">
         <div class="custom-select">
           <select id="tipo-mensagem" name="Tipo de Mensagem">
@@ -55,14 +56,15 @@
             <option value="3">Direção</option>
           </select>
         </div>
-      <textarea type="text" name="mensagem" id="mensagem-ct" placeholder="Mensagem" rows="8"></textarea>
-      <div class="submit-container">
-        <label for="submit" class="submit-file-ct btn-hover">Enviar mensagem</label>
-        <input type="submit" value="Enviar minhas informações" name="submit" id="submit-ct">
-      </div>
-    </form>
-  </div>
-</section>
+        <textarea type="text" name="mensagem" id="mensagem-ct" placeholder="Mensagem" rows="8"></textarea>
+        <div class="submit-container">
+          <label for="submit" class="submit-file-ct btn-hover">Enviar mensagem</label>
+          <input type="submit" value="Enviar minhas informações" name="submit" id="submit-ct">
+        </div>
+      </form>
+    </div>
+  </section>
+</main>
 
 <!-- JavaScript para Dropdown de tipos de mensagem -->
 <script>
@@ -87,39 +89,40 @@
       c = document.createElement("DIV");
       c.innerHTML = selElmnt.options[j].innerHTML;
       c.addEventListener("click", function(e) {
-          /*when an item is clicked, update the original select box,
-          and the selected item:*/
-          var y, i, k, s, h, sl, yl;
-          s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-          sl = s.length;
-          h = this.parentNode.previousSibling;
-          for (i = 0; i < sl; i++) {
-            if (s.options[i].innerHTML == this.innerHTML) {
-              s.selectedIndex = i;
-              h.innerHTML = this.innerHTML;
-              y = this.parentNode.getElementsByClassName("same-as-selected");
-              yl = y.length;
-              for (k = 0; k < yl; k++) {
-                y[k].removeAttribute("class");
-              }
-              this.setAttribute("class", "same-as-selected");
-              break;
+        /*when an item is clicked, update the original select box,
+        and the selected item:*/
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
             }
+            this.setAttribute("class", "same-as-selected");
+            break;
           }
-          h.click();
+        }
+        h.click();
       });
       b.appendChild(c);
     }
     x[i].appendChild(b);
     a.addEventListener("click", function(e) {
-        /*when the select box is clicked, close any other select boxes,
-        and open/close the current select box:*/
-        e.stopPropagation();
-        closeAllSelect(this);
-        this.nextSibling.classList.toggle("select-hide");
-        this.classList.toggle("select-arrow-active");
-      });
+      /*when the select box is clicked, close any other select boxes,
+      and open/close the current select box:*/
+      e.stopPropagation();
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle("select-hide");
+      this.classList.toggle("select-arrow-active");
+    });
   }
+
   function closeAllSelect(elmnt) {
     /*a function that will close all select boxes in the document,
     except the current select box:*/
--- page-inicio.php
+++ page-inicio.php
@@ -1,117 +1,121 @@
-<?php // Template Name: Inicio ?>
+<?php // Template Name: Inicio 
+?>
 
 <?php get_header(); ?>
 
-<div class="carousel">
-  <img  alt="Logo weblaudo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/IMG-destaque-WL-03.jpg"/>
-  <div class="float-box-home1">
-    <h1 class="uppercase">Teleradiologia</h1>
-    <h4>
-      Laudos a distância e segunda opinião especalizada através da telemedicina que permite a elaboração de resultados precisos e eficientes.
-    </h4>
-    <div class="button-wrapper-home1">
-      <button class="btn-index-sec1 btn1 btn-hover">Solicitar orçamento</button>
-      <button class="btn-index-sec1 btn2 btn-hover">Fale conosco</button>
+<main class="inicio">
+  <div class="carousel">
+    <img alt="Logo weblaudo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/IMG-destaque-WL-03.jpg" />
+    <div class="float-box-home1">
+      <h1 class="uppercase">Teleradiologia</h1>
+      <h4>
+        Laudos a distância e segunda opinião especalizada através da telemedicina que permite a elaboração de resultados precisos e eficientes.
+      </h4>
+      <div class="button-wrapper-home1">
+        <button class="btn-index-sec1 btn1 btn-hover">Solicitar orçamento</button>
+        <button class="btn-index-sec1 btn2 btn-hover">Fale conosco</button>
+      </div>
     </div>
+    <a class="wp-icon btn-hover"><img alt="wp-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/whatsapp.png" /></a>
   </div>
-  <a class="wp-icon btn-hover"><img  alt="wp-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/whatsapp.png"/></a>
-</div>
 
-<section class="servicos">
-  <h1>Obtenha laudos a distância em radiologia</h1>
-  <h4>A <strong>Weblaudo</strong> associou a melhor estrutura tecnológica em transmissões de imagem </br>
-    a uma renomada equipe de radiologistas de ampla experiência profissional.</h4>
-  <div class="servicos-divider"></div>
-  <div class="servicos-content">
-    <ul>
-      <li>
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-1.jpg">
-        <h5>Exame</h5>
-        <p>Realização de exames em hospitais e centros de imagem de todo país.</p>
-      </li>
-      <li>
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-2.jpg">
-        <h5>Envio</h5>
-        <p>Envio automático e seguro das imagens via internet.</p>
-      </li>
-      <li>
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-3.jpg">
-        <h5>Laudo</h5>
-        <p>Elaboração dos laudos a distância por radiologistas especialistas, 24h/dia 365 dias/ano.</p>
-      </li>
-      <li>
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-4.jpg">
-        <h5>Entregado</h5>
-        <p>Liberação automática dos laudos a distância no própio local ou via plataforma.</p>
-      </li>
-      <li>
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-5.jpg">
-        <h5>Sistema</h5>
-        <p>Laudos e imagens disponíveis para acesso de pacientes e médicos de qualquer lugar.</p>
-      </li>
-    </ul>
-  </div>
-  <div class="servicos-divider-2"></div>
-  <p class="servicos-p">Em menos de 1 hora, seu serviço de imagem já pode receber nossos laudos a distância</p>
-  <button class="btn-contato-orange btn-hover">Entre em contato!</button>
-</section>
+  <section class="servicos">
+    <h1>Obtenha laudos a distância em radiologia</h1>
+    <h4>A <strong>Weblaudo</strong> associou a melhor estrutura tecnológica em transmissões de imagem </br>
+      a uma renomada equipe de radiologistas de ampla experiência profissional.</h4>
+    <div class="servicos-divider"></div>
+    <div class="servicos-content">
+      <ul>
+        <li>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-1.jpg">
+          <h5>Exame</h5>
+          <p>Realização de exames em hospitais e centros de imagem de todo país.</p>
+        </li>
+        <li>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-2.jpg">
+          <h5>Envio</h5>
+          <p>Envio automático e seguro das imagens via internet.</p>
+        </li>
+        <li>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-3.jpg">
+          <h5>Laudo</h5>
+          <p>Elaboração dos laudos a distância por radiologistas especialistas, 24h/dia 365 dias/ano.</p>
+        </li>
+        <li>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-4.jpg">
+          <h5>Entregado</h5>
+          <p>Liberação automática dos laudos a distância no própio local ou via plataforma.</p>
+        </li>
+        <li>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-5.jpg">
+          <h5>Sistema</h5>
+          <p>Laudos e imagens disponíveis para acesso de pacientes e médicos de qualquer lugar.</p>
+        </li>
+      </ul>
+    </div>
+    <div class="servicos-divider-2"></div>
+    <p class="servicos-p">Em menos de 1 hora, seu serviço de imagem já pode receber nossos laudos a distância</p>
+    <button class="btn-contato-orange btn-hover">Entre em contato!</button>
+  </section>
 
-<section class="index-sec-3">
-  <div class="sec-3-banner-container">
-    <img class="index-sec-3-banner" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/IMG-destaque-WL-02.jpg">
-  </div>
-  <div class="sec-3-content">
-    <h1>Laudos a distância em Radiologia </br>para clínicas e hospitais</h1>
-    <h4>A telerradiologia é o ramo específico da telemedicina que permite a elaboração de laudos a distância (telelaudo) e segunda
-       opinião especializada.
-    </h4>
-    <ul class="sec-3-ul">
-      <li>Tomografia Computadorizada</li>
-      <li>Ressonância Magnética</li>
-      <li>Laudos de Segunda Opinião</li>
-      <li>Raio-x</li>
-      <li>Densitometria Óssea</li>
-      <li>Mamografia</li>
-      <li>Laudos em até 2 horas</li>
-    </ul>
-  </div>
-</section>
+  <section class="index-sec-3">
+    <div class="sec-3-banner-container">
+      <img class="index-sec-3-banner" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/IMG-destaque-WL-02.jpg">
+    </div>
+    <div class="sec-3-content">
+      <h1>Laudos a distância em Radiologia </br>para clínicas e hospitais</h1>
+      <h4>A telerradiologia é o ramo específico da telemedicina que permite a elaboração de laudos a distância (telelaudo) e segunda
+        opinião especializada.
+      </h4>
+      <ul class="sec-3-ul">
+        <li>Tomografia Computadorizada</li>
+        <li>Ressonância Magnética</li>
+        <li>Laudos de Segunda Opinião</li>
+        <li>Raio-x</li>
+        <li>Densitometria Óssea</li>
+        <li>Mamografia</li>
+        <li>Laudos em até 2 horas</li>
+      </ul>
+    </div>
+  </section>
 
-<section class="faq">
+  <section class="faq">
 
-        <h2>Perguntas Frequentes</h2>
+    <h2>Perguntas Frequentes</h2>
 
-        <?php if ( have_rows('FAQ') ) : $i = 0; ?>
+    <?php if (have_rows('FAQ')) : $i = 0; ?>
 
-        <?php while (have_rows('FAQ')) : the_row(); $i++;?>
+      <?php while (have_rows('FAQ')) : the_row();
+        $i++; ?>
 
         <div class="accordion">
-            <div class="accordion-item">
-                <div class="accordion-item-header">
-                    <h3>
-                        <span><?php echo '0' . $i . '.' ?></span>
-                        <?php the_sub_field('question_tittle'); ?>
-                    </h3>
-                    <i class="fa-solid fa-chevron-down"></i>
-                </div>
-                <div class="accordion-item-body">
-                  <div class="accordion-item-body-content">
-                      <?php the_sub_field('question_answer'); ?>
-                  </div>
-                </div>
+          <div class="accordion-item">
+            <div class="accordion-item-header">
+              <h3>
+                <span><?php echo '0' . $i . '.' ?></span>
+                <?php the_sub_field('question_tittle'); ?>
+              </h3>
+              <i class="fa-solid fa-chevron-down"></i>
             </div>
+            <div class="accordion-item-body">
+              <div class="accordion-item-body-content">
+                <?php the_sub_field('question_answer'); ?>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <?php endwhile; ?>
+      <?php endwhile; ?>
 
-        <div class="duvida">
-            <p>Não tirou sua dúvida? Pergunte agora > > ></p><button>PERGUNTE AQUI</button>
-        </div>
+      <div class="duvida">
+        <p>Não tirou sua dúvida? Pergunte agora > > ></p><button>PERGUNTE AQUI</button>
+      </div>
 
-      <?php else: echo 'Ainda não há perguntas'; endif; ?>
-</section>
+    <?php else : echo 'Ainda não há perguntas';
+    endif; ?>
+  </section>
 
-<section class="work-with-us">
+  <section class="work-with-us">
     <h1>Trabalhe Conosco</h1>
     <h6>Agradecemos o seu interesse em fazer parte da equipe WebLaudo! </br> Anexe seu currículo que entraremos em contato.</h6>
 
@@ -121,9 +125,10 @@
           <input type="text" name="hosp" id="hosp" placeholder="Nome do hospital ou clinica*">
           <input type="text" name="nome" id="nome" placeholder="Seu nome*">
         </div>
+
         <div class="custom-select-container">
+          <input type="text" name="cidade" id="cidade" placeholder="Cidade*">
           <div class="custom-select">
-            <input type="text" name="cidade" id="cidade" placeholder="Cidade*">
             <select id="estado" name="estado">
               <option value="AC">Acre</option>
               <option value="AC">Acre</option>
@@ -159,7 +164,7 @@
         </div>
         <div>
           <input type="email" name="email" id="email" placeholder="Email*">
-          <input type="tel" name="tel" id="tel"  placeholder="DDD e Telefone*">
+          <input type="tel" name="tel" id="tel" placeholder="DDD e Telefone*">
         </div>
         <textarea type="text" name="mensagem" id="mensagem" placeholder="Mensagem" rows="8"></textarea>
         <div>
@@ -174,6 +179,7 @@
       </form>
     </div>
   </section>
+</main>
 
 <!-- JavaScript para Dropdown de tipos de mensagem -->
 <script>
@@ -198,39 +204,40 @@
       c = document.createElement("DIV");
       c.innerHTML = selElmnt.options[j].innerHTML;
       c.addEventListener("click", function(e) {
-          /*when an item is clicked, update the original select box,
-          and the selected item:*/
-          var y, i, k, s, h, sl, yl;
-          s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-          sl = s.length;
-          h = this.parentNode.previousSibling;
-          for (i = 0; i < sl; i++) {
-            if (s.options[i].innerHTML == this.innerHTML) {
-              s.selectedIndex = i;
-              h.innerHTML = this.innerHTML;
-              y = this.parentNode.getElementsByClassName("same-as-selected");
-              yl = y.length;
-              for (k = 0; k < yl; k++) {
-                y[k].removeAttribute("class");
-              }
-              this.setAttribute("class", "same-as-selected");
-              break;
+        /*when an item is clicked, update the original select box,
+        and the selected item:*/
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
             }
+            this.setAttribute("class", "same-as-selected");
+            break;
           }
-          h.click();
+        }
+        h.click();
       });
       b.appendChild(c);
     }
     x[i].appendChild(b);
     a.addEventListener("click", function(e) {
-        /*when the select box is clicked, close any other select boxes,
-        and open/close the current select box:*/
-        e.stopPropagation();
-        closeAllSelect(this);
-        this.nextSibling.classList.toggle("select-hide");
-        this.classList.toggle("select-arrow-active");
-      });
+      /*when the select box is clicked, close any other select boxes,
+      and open/close the current select box:*/
+      e.stopPropagation();
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle("select-hide");
+      this.classList.toggle("select-arrow-active");
+    });
   }
+
   function closeAllSelect(elmnt) {
     /*a function that will close all select boxes in the document,
     except the current select box:*/
