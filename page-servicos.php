835f7c13d51ecc2e8f933c6ae209d6bb7c176854
Rafael <rafaelvrcr@gmail.com>
2022-08-16 15:03:38 -0300

    [Fix] Header box blue

--- page-servicos.php
+++ page-servicos.php
@@ -2,105 +2,107 @@
 ?>
 
 <?php get_header(); ?>
-<section class="container-banner-servicos">
-  <div class="banner-title">
-    <h1>Serviços</h1>
-  </div>
-  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/servicos.jpg" alt="">
-  <div class="banner-servicos">
-    <div>
-      <h1>Serviço de </h1>
-      <h2>Telerradiologia</h2>
+<main class="servicos">
+  <section class="container-banner-servicos">
+    <div class="banner-title">
+      <h1>Serviços</h1>
     </div>
-    <h3>Para otimizar o atendimento</h3>
-  </div>
-</section>
-<section class="container-sub-content-servicos">
-  <p>
-    Com os nossos serviços de telerradiologia você encontra a forma que precisa
-    para otimizar o atendimento aos seus pacientes, ampliando o horário, garantindo
-    a rapidez na entrega dos laudos e diminuindo a alta carga das análises de imagem
-    pelos médicos do seu corpo clínico. Sua equipe poderá dispor ainda de uma segunda
-    opinião em casos de maior complexidade, garantindo maior qualidade e confiabilidade
-    nos relatórios radiológicos. Também prestamos consultoria na administração e
-    montagem dos equipamentos de imagem, incluindo a escolha e a compra dos aparelhos,
-    treinamento de equipes técnicas e palestras.
-  </p>
-</section>
-
-<section class="page-servicos-content">
-  <div class="card-conteiner-servicos  addon-card-servicos">
-    <div class="card-servicos-1">
-      <div class="text-container-somos">
-        <h2 class="como-h2">Nossos</h2>
-        <h2 class="strong-h2">Serviços</h2>
-        <div class="como-funciona-line"></div>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/servicos.jpg" alt="">
+    <div class="banner-servicos">
+      <div>
+        <h1>Serviço de </h1>
+        <h2>Telerradiologia</h2>
       </div>
-      <ul>
-        <li>Tecnologia de transmissão</li>
-        <li>Radiologistas experientes, comprometidos
-          e especializados</li>
-        <li>Segurança total na transmissão das imagens</li>
-        <li>Consultoria para novos serviços ou
-          equipamentos</li>
-        <li>Compromisso com a qualidade</li>
-        <li>Protocolo de exames</li>
-        <li>Treinamento técnico</li>
-      </ul>
+      <h3>Para otimizar o atendimento</h3>
     </div>
-    <div class="card-servicos-2">
-      <div class="text-container-somos">
-        <h2 class="como-h2">Nossos</h2>
-        <h2 class="strong-h2">diferenciais</h2>
-        <div class="como-funciona-line-2"></div>
-      </div>
-      <ul>
-        <li>Protocolos e treinamento técnico</li>
-        <li>audos já digitados (prontos para entrega)</li>
-        <li>Remuneração por laudo</li>
-        <li>Sem riscos trabalhistas</li>
-        <li>Sem custo de manutenção</li>
-        <li>Possibilita ampliar o horário de
-          atendimento do serviço</li>
-        <li>Atendimento personalizado</li>
-      </ul>
-    </div>
-  </div>
-  <section class="servicos addon-servicos">
-    <div class="servicos-content">
-      <ul>
-        <li>
-          <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-1.jpg">
-          <h5>Exame</h5>
-          <p>Realização de exames em hospitais e centros de imagem de todo país.</p>
-        </li>
-        <li>
-          <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-2.jpg">
-          <h5>Envio</h5>
-          <p>Envio automático e seguro das imagens via internet.</p>
-        </li>
-        <li>
-          <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-3.jpg">
-          <h5>Laudo</h5>
-          <p>Elaboração dos laudos a distância por radiologistas especialistas, 24h/dia 365 dias/ano.</p>
-        </li>
-        <li>
-          <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-4.jpg">
-          <h5>Entregado</h5>
-          <p>Liberação automática dos laudos a distância no própio local ou via plataforma.</p>
-        </li>
-        <li>
-          <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-5.jpg">
-          <h5>Sistema</h5>
-          <p>Laudos e imagens disponíveis para acesso de pacientes e médicos de qualquer lugar.</p>
-        </li>
-      </ul>
-    </div>
-    <div class="servicos-divider-2 "></div>
-    <p class="servicos-p">Em menos de 1 hora, seu serviço de imagem já pode receber nossos laudos a distância</p>
-    <button class="btn-contato-orange btn-hover">Entre em contato!</button>
   </section>
-</section>
+  <section class="container-sub-content-servicos">
+    <p>
+      Com os nossos serviços de telerradiologia você encontra a forma que precisa
+      para otimizar o atendimento aos seus pacientes, ampliando o horário, garantindo
+      a rapidez na entrega dos laudos e diminuindo a alta carga das análises de imagem
+      pelos médicos do seu corpo clínico. Sua equipe poderá dispor ainda de uma segunda
+      opinião em casos de maior complexidade, garantindo maior qualidade e confiabilidade
+      nos relatórios radiológicos. Também prestamos consultoria na administração e
+      montagem dos equipamentos de imagem, incluindo a escolha e a compra dos aparelhos,
+      treinamento de equipes técnicas e palestras.
+    </p>
+  </section>
+  
+  <section class="page-servicos-content">
+    <div class="card-conteiner-servicos  addon-card-servicos">
+      <div class="card-servicos-1">
+        <div class="text-container-somos">
+          <h2 class="como-h2">Nossos</h2>
+          <h2 class="strong-h2">Serviços</h2>
+          <div class="como-funciona-line"></div>
+        </div>
+        <ul>
+          <li>Tecnologia de transmissão</li>
+          <li>Radiologistas experientes, comprometidos
+            e especializados</li>
+          <li>Segurança total na transmissão das imagens</li>
+          <li>Consultoria para novos serviços ou
+            equipamentos</li>
+          <li>Compromisso com a qualidade</li>
+          <li>Protocolo de exames</li>
+          <li>Treinamento técnico</li>
+        </ul>
+      </div>
+      <div class="card-servicos-2">
+        <div class="text-container-somos">
+          <h2 class="como-h2">Nossos</h2>
+          <h2 class="strong-h2">diferenciais</h2>
+          <div class="como-funciona-line-2"></div>
+        </div>
+        <ul>
+          <li>Protocolos e treinamento técnico</li>
+          <li>audos já digitados (prontos para entrega)</li>
+          <li>Remuneração por laudo</li>
+          <li>Sem riscos trabalhistas</li>
+          <li>Sem custo de manutenção</li>
+          <li>Possibilita ampliar o horário de
+            atendimento do serviço</li>
+          <li>Atendimento personalizado</li>
+        </ul>
+      </div>
+    </div>
+    <section class="servicos addon-servicos">
+      <div class="servicos-content">
+        <ul>
+          <li>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-1.jpg">
+            <h5>Exame</h5>
+            <p>Realização de exames em hospitais e centros de imagem de todo país.</p>
+          </li>
+          <li>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-2.jpg">
+            <h5>Envio</h5>
+            <p>Envio automático e seguro das imagens via internet.</p>
+          </li>
+          <li>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-3.jpg">
+            <h5>Laudo</h5>
+            <p>Elaboração dos laudos a distância por radiologistas especialistas, 24h/dia 365 dias/ano.</p>
+          </li>
+          <li>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-4.jpg">
+            <h5>Entregado</h5>
+            <p>Liberação automática dos laudos a distância no própio local ou via plataforma.</p>
+          </li>
+          <li>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icon-5.jpg">
+            <h5>Sistema</h5>
+            <p>Laudos e imagens disponíveis para acesso de pacientes e médicos de qualquer lugar.</p>
+          </li>
+        </ul>
+      </div>
+      <div class="servicos-divider-2 "></div>
+      <p class="servicos-p">Em menos de 1 hora, seu serviço de imagem já pode receber nossos laudos a distância</p>
+      <button class="btn-contato-orange btn-hover">Entre em contato!</button>
+    </section>
+  </section>
+</main>
 
 
 <?php get_footer(); ?>
