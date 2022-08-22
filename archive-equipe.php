4b1efa281219b6e2a16af1f6ffd76b88c6b11ecf
Rafael <rafaelvrcr@gmail.com>
2022-08-22 11:40:45 -0300

    [Feat] Work with us

--- archive-equipe.php
+++ archive-equipe.php
@@ -20,14 +20,20 @@
               <img class="thumb-post-banco" src="<?php the_field('foto') ?>" />
               <h2 class="thumb-title-banco">
                 <a href="<?php the_permalink(); ?>" ?><?php the_title(); ?></a>
-              </h2>
-              <h6 class="excerpt-banco"><?php the_excerpt(); ?></h6>
-              <h6 class="saiba-mais-banco">
-                <a class="saiba-mais-banco-a" href="<?php the_permalink(); ?>" ?>SAIBA MAIS</a>
-              </h6>
+              </h2>              
+              <h6 class="excerpt-banco"><?php the_field('especialidade') ?></h6>
+              <button class="saiba-mais-banco">
+                <a class="saiba-mais-banco-a" href="<?php the_permalink(); ?>" ?>MAIS INFORMAÇÕES</a>
+              </button>
             </article>
           <?php endwhile; ?>
         <?php else : endif; ?>
+      </section>
+      <section class="work-with-us">
+        <h1>Faça parte do time exclusivo de mestres da WebLaudo.</h1>
+        <button class="btn-work-with-us">
+          TRABALHE CONOSCO
+        </button>
       </section>
     </div>
   </div>
